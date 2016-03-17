8fb0ba354b1b638ad12e050d81bb00e2147a1faf
Stuart Runyan <shrunyan@gmail.com>
2016-03-17 15:12:48 -0700

    Feat: Allow providing a custom method prefix

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 const codes = [200, 201, 202, 400, 401, 402, 403, 404, 408, 500, 501, 502]
 
-module.exports = (req, res, next) => {
-  codes.forEach((code) => {
-    let response = require(`./${code}`)
-    res[`res${code}`] = response.bind(res)
-  })
+module.exports = function response(prefix) {
+  prefix = prefix || 'res'
+  return (req, res, next) => {
+    codes.forEach((code) => {
+      let response = require(`./${code}`)
+      res[`${prefix}${code}`] = response.bind(res)
+    })
 
-  next()
+    next()
+  }
 }
