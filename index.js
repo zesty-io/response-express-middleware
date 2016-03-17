2050bb624680bd8189cc6d3afae3f0170df3f54d
Stuart Runyan <shrunyan@gmail.com>
2016-03-16 21:00:20 -0700

    style: Fix lint errors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const codes = [200,201,202,400,401,402,403,404,408,500,501,502]
+const codes = [200, 201, 202, 400, 401, 402, 403, 404, 408, 500, 501, 502]
 
 module.exports = (req, res, next) => {
-  codes.forEach(code => {
+  codes.forEach((code) => {
     let response = require(`./${code}`)
     res[`res${code}`] = response.bind(res)
   })
