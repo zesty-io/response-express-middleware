d677c794bcbe7c876c180349c4d958b5c89c20f1
Stuart Runyan <shrunyan@gmail.com>
2016-08-23 12:32:48 -0700

    Simplfying the setup for respond arguments

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict'
 
+/**
+ * Middleware that binds our .respond() methods
+ *
+ * Note: for .respond(), data cannot be a string (because it will be interpreted as the message)
+ *
+ * Usage:
+ *
+ * res.status(201).respond({message: 'custom response overrides', data: data, specialProp: true})
+ * res.status(201).respond('This was totally created')
+ * res.status(201).respond('Check this out', data)
+ * res.status(201).respond(data)
+ * res.respond('Here is some data', data)
+ * res.respond('Your request was totally cool')
+ * res.respond(data)
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+module.exports = function respond(req, res, next) {
+  res.respond = customResponse.bind(res)
+  next()
+}
+
 const statusMessages = {
   200: 'OK',
   201: 'Created',
@@ -24,83 +48,49 @@ const statusMessages = {
 }
 
 /**
- * Send a response and override defaults in our base response
- *
- * @param {object} [overrides]
- */
-const respondCustom = function(overrides = {}) {
-  let data = Object.assign({
-    message: '',
-    status: '',
-    data: null
-  }, overrides)
-
-  // Set a property "code" on the response from our current status code
-  data.code = this.statusCode
-
-  // If there isn't a message already, try to populate a default message for the status code
-  if (data.status === '') {
-    if (statusMessages.hasOwnProperty(this.statusCode)) {
-      data.status = statusMessages[this.statusCode]
-    }
-  }
-
-  // Send the response
-  this.send(data)
-
-}
-
-/**
  * Send a response and optionally set the message and/or data
  *
  * @param {string} [message]
  * @param {object} [data]
  */
-const respond = function() {
-
-  let userOverrides = {}
-
-  // If there are two arguments, we expect them to be (message: String, objectProps: Object)
-  if (arguments.length === 2) {
-    userOverrides = arguments[1]
-    userOverrides.message = arguments[0]
+function customResponse(msg, body) {
+  let response = {
+    message: '',
+    status: '',
+    data: null
   }
 
-  // If there is one argument, we will accept either message:String OR objectProps:Any-non-string
-  else if (arguments.length === 1) {
-    if (typeof arguments[0] === typeof '') {
-      userOverrides.message = arguments[0]
-    } else if (typeof arguments[0] === typeof {}) {
-      userOverrides = {data: arguments[0]}
+  if (typeof msg === typeof '') {
+    response.message = msg
+
+    // When message is a string and body was
+    // supplied, send it as data
+    if (body) {
+      response.data = body
+    }
+
+  } else {
+    if (msg.data || msg.message) {
+      // When these properties are present the intent
+      // is to override the whole response
+      response = Object.assign(response, msg)
+    } else {
+      // Just set the message as data
+      response.data = msg
     }
   }
 
-  respondCustom.call(this, userOverrides)
+  // If there isn't a status already, try to populate
+  // a default status for the status code
+  if (response.status === '') {
+    if (statusMessages.hasOwnProperty(this.statusCode)) {
+      response.status = statusMessages[this.statusCode]
+    }
+  }
 
-}
+  // Status codes must be set by
+  // expressjs `status()` method
+  response.code = this.statusCode
 
-/**
- * Middleware that binds our .respond() and .respondCustom() methods
- *
- * Note: for .respond(), data cannot be a string (because it will be interpreted as the message)
- *
- * Usage:
- *
- * res.status(201).respondCustom({message: 'custom response overrides', data: data, specialProp: true})
- *
- * res.status(201).respond('This was totally created')
- * res.status(201).respond('Check this out', data)
- * res.status(201).respond(data)
- * res.respond('Here is some data', data)
- * res.respond('Your request was totally cool')
- * res.respond(data)
- *
- * @param req
- * @param res
- * @param next
- */
-module.exports = function bindRespondMethods(req, res, next) {
-  res.respond = respond.bind(res)
-  res.respondCustom = respondCustom.bind(res)
-  next()
+  this.send(response)
 }
